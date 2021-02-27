1b856aa4c59d1d3b1dc84fe2fdbd25af24aa002d
Andreson Costa <alcf2@discente.ifpe.edu.br>
2021-02-27 10:54:45 -0300

    First commit de calculadora2

---
+++ R/divisao.R
@@ -0,0 +1,16 @@
+#' Função para dividir.
+#'
+#' Esta função serve para dividir um numero por outro
+#'
+#'@param a, numero que será dividido
+#'@param b, outro número que irar dividir
+#'
+#'@examples
+#'
+#'divisao(4,2)
+#'resposta 2
+#'
+#' @export
+divisao <- function(a,b) {
+  a/b
+}
---
+++ R/multiplicacao.R
@@ -0,0 +1,17 @@
+#'
+#' Função para multiplicar.
+#'
+#' Esta função serve para multiplicar dois números
+#'
+#'@param a, numero a multiplicar
+#'@param b, outro número a multiplicar
+#'
+#'@examples
+#'
+#'multiplicacao(4,2)
+#'resposta 8
+#'
+#' @export
+multiplicacao <- function(a,b) {
+  a*b
+}
